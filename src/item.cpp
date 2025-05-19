e533703551dce35b23d3789c47a2cdf42d42da30
Alexei Robyn <shados@shados.net>
2025-05-20 01:04:10 +1000

    Make NO_WEAR_EFFECT items ignored for rigidity calculation
    Given the intended use of the flag, this makes sense, and largely
    ensures that jewelry items don't cause rigidity conflicts.

--- src/item.cpp
+++ src/item.cpp
@@ -15236,7 +15236,8 @@ template <typename T>
 bool item::is_bp_rigid( const T &bp ) const
 {
     // overrides for the item overall
-    if( has_flag( flag_SOFT ) ) {
+    // NO_WEAR_EFFECT is there for jewelry and the like which is too small to be considered
+    if( has_flag( flag_SOFT ) || has_flag( flag_NO_WEAR_EFFECT ) ) {
         return false;
     } else if( has_flag( flag_HARD ) ) {
         return true;
@@ -15281,7 +15282,8 @@ bool item::is_bp_rigid_selective( const T &bp ) const
     }
 
     // overrides for the item overall
-    if( has_flag( flag_SOFT ) ) {
+    // NO_WEAR_EFFECT is there for jewelry and the like which is too small to be considered
+    if( has_flag( flag_SOFT ) || has_flag( flag_NO_WEAR_EFFECT ) ) {
         is_rigid = false;
     } else if( has_flag( flag_HARD ) ) {
         is_rigid = true;
